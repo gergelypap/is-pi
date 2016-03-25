13b1c12adb8978a307a8c81d4dcef4348724caa6
Gergely Pap <gpap@brainsum.com>
2016-03-25 15:27:53 +0100

    added more tests

--- test.js
+++ test.js
@@ -5,12 +5,23 @@ var expect = require('chai').expect;
 
 // Describe our tests.
 describe('fake suite of tests', function() {
-
 	it('Math.PI should be PI', function() {
 		expect(isPI(Math.PI)).to.equal(true);
 	});
 
 	it('3 should not be PI', function() {
 		expect(isPI(3)).to.equal(false);
+	});
+
+	it('3.14 should be PI', function() {
+		expect(isPI(3.14)).to.equal(true);
+	});
+
+	it('unicorn should not be PI', function() {
+		expect(isPI('unicorn')).to.equal(false);
+	});
+
+	it('3.13 should be almost PI', function() {
+		expect(isPI(3.13)).to.equal(false);
 	});
 });
