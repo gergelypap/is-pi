c7d0413b72ae954295d3c410289dcaa153425c49
Gergely Pap <pgege42@gmail.com>
2016-03-26 11:43:43 +0100

    fixed issues by adding predefined values

--- src/index.js
+++ src/index.js
@@ -9,4 +9,26 @@
  * @return {Boolean}
  *   Whether x is PI or not.
  */
-export default (x) => x == 3.14 ? true : Math.PI === +parseFloat(x).toFixed(15)
+export default (x) => {
+  if ([
+    3.14,
+    3.141,
+    3.1415,
+    3.14159,
+    3.141592,
+    3.1415926,
+    3.14159265,
+    3.141592653,
+    3.1415926535,
+    3.14159265358,
+    3.141592653589,
+    3.1415926535897,
+    3.14159265358979,
+    'PI',
+    'pi',
+    'Pi',
+    'π',
+    'Π',
+  ].contains(x)) return true
+  else return Math.PI === +parseFloat(x).toFixed(15)
+}
